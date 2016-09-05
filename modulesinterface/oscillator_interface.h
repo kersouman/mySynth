16bd82089d0ba78b4f43f130b60c9c8bfc7b6a7d
Thomas Chevet <chevet.thomas@gmail.com>
2016-09-05 23:27:45 +0200

    Added waveform to oscillator behavior

--- modulesinterface/oscillator_interface.h
+++ modulesinterface/oscillator_interface.h
@@ -1,5 +1,10 @@
 #ifndef DEF_OSCILLATOR_INTERFACE
 #define DEF_OSCILLATOR_INTERFACE
 
+typedef struct Wave {
+    unsigned short waveType : 2;    // Since we have sine, sawtooth, triangle and square we only need 4 values
+    unsigned long frequency : 24;   // The frequency of the signal
+    unsigned int pulseWidth : 8;    // Used only if we have a square signal
+}
 
 #endif
